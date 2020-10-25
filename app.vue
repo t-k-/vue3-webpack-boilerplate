266b4768a5dfc564b31b424c91e8a1b304af431c
t-k- <clock126@126.com>
2020-10-25 23:14:47 +0800

    bug?

--- app.vue
+++ app.vue
@@ -1,26 +1,13 @@
 <template>
-  <Button @click="toggle">Click me!</Button>
 
-  <Dialog header="Header" v-model:visible="display" >
-    Content
-  </Dialog>
+<Card>
+Content
+</Card>
+
 </template>
 
 <script>
-module.exports = {
-  data: function() {
-    return {
-      display: false
-    }
-  },
-
-  methods: {
-    toggle() {
-      this.display = !this.display
-    }
-  }
-}
 </script>
 
-<style scoped>
+<style>
 </style>
--- main.js
+++ main.js
@@ -4,10 +4,8 @@ import App from './app.vue'
 
 const app = createApp(App)
 
-import Dialog from 'primevue/dialog'
-import Button from 'primevue/button'
-app.component('Dialog', Dialog)
-app.component('Button', Button)
+import Card from 'primevue/card';
+app.component('Card', Card)
 
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
