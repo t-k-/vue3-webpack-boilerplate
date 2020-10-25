32a0b91972c656e659df348e04ba982922d9b9fa
t-k- <clock126@126.com>
2020-10-25 21:58:06 +0800

    overwrite font-size and font-family.

--- main.js
+++ main.js
@@ -13,9 +13,10 @@ app.component('Button', Button)
 app.component('Toolbar', Toolbar)
 app.component('InputText', InputText)
 
-import './background.css'
-import 'primeflex/primeflex.css'
-import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeflex/primeflex.css'
+import './linux-libertine-webfont.css'
+import './background.css'
 
 app.mount('#app')
